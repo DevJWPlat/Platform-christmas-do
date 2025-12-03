461d73ba38928e859c1bb9787e1818bfee04a895
DevJWPlat <166898654+DevJWPlat@users.noreply.github.com>
2025-12-03 21:27:49 +0000

    Sure! Pl

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  // This is IMPORTANT for GitHub Pages if you don't use a custom domain yet
+  base: '/Platform-christmas-do/',
+
+  build: {
+    outDir: 'docs',
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
